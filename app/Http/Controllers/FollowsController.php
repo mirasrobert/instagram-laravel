37469d01ff9b20bb14d7ddcfb73e7c4ce81dd1c7
mirasrobert <mrmirasrobert@gmail.com>
2022-12-29 17:31:58 +0800

    Limit friends in news feed home

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -20,12 +20,14 @@ class FollowsController extends Controller
         // This will get all the user_id in profiles table using the many-to-many relationship
         // Return [1, 2, 3] array of user_id
         $following = auth()->user()->following()->pluck('profiles.user_id');
-        $users = User::whereIn('id', $following)->get();
+        $users = User::whereIn('id', $following)
+            ->take(3)
+            ->get();
 
         // Put ur id in collection to remove from suggestion follwing
         $following->push(auth()->user()->id);
 
-        // Get 5 users that you are not following 
+        // Get 5 users that you are not following
         $suggestions = User::whereNotIn('id', $following)
             ->take(4)
             ->get();
